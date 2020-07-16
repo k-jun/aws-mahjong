6ccfce49ae3d58cc2d4c36ba8dcbf6a8037c8121
keijun.kumagai <keijun091221@gmail.com>
2020-07-17 00:28:36 +0900

    feat(usecase): create room usecase

--- game/game.go
+++ game/game.go
@@ -6,3 +6,15 @@ type Game struct {
 	capacity int
 	board    *board.Board
 }
+
+func NewGame(capacity int) *Game {
+	return &Game{
+		capacity: capacity,
+		board:    nil,
+	}
+}
+
+func (g *Game) Capacity() int {
+	return g.capacity
+
+}
---
+++ usecase/room_usecase.go
@@ -0,0 +1,72 @@
+package usecase
+
+import (
+	"aws-mahjong/game"
+	"aws-mahjong/repository"
+	"errors"
+
+	socketio "github.com/googollee/go-socket.io"
+)
+
+var (
+	RoomAlraedyTokenErr = errors.New("room already token")
+	RoomNotFound        = errors.New("room is not found")
+	RoomReachMaxMember  = errors.New("room already fulled")
+)
+
+type RoomUsecase struct {
+	gameRepo repository.GameRepository
+	roomRepo repository.RoomRepository
+}
+
+func NewRoomUsecase(gameRepo repository.GameRepository, roomRepo repository.RoomRepository) *RoomUsecase {
+	return &RoomUsecase{
+		gameRepo: gameRepo,
+		roomRepo: roomRepo,
+	}
+}
+
+func (u *RoomUsecase) CreateRoom(s socketio.Conn, roomName string, roomCapacity int) error {
+
+	if u.roomRepo.RoomLen(roomName) != 0 {
+		return RoomAlraedyTokenErr
+	}
+	u.roomRepo.JoinRoom(s, roomName)
+	err := u.gameRepo.Add(roomName, game.NewGame(roomCapacity))
+	return err
+}
+
+func (u *RoomUsecase) JoinRoom(s socketio.Conn, roomName string) error {
+
+	if u.roomRepo.RoomLen(roomName) == 0 {
+		return RoomNotFound
+	}
+
+	game, err := u.gameRepo.Find(roomName)
+	if err != nil {
+		return RoomNotFound
+	}
+	if u.roomRepo.RoomLen(roomName) >= game.Capacity() {
+		return RoomReachMaxMember
+	}
+
+	u.roomRepo.JoinRoom(s, roomName)
+	return nil
+}
+
+func (u *RoomUsecase) LeaveRoom(s socketio.Conn, roomName string) error {
+	u.roomRepo.LeaveRoom(s, roomName)
+	err := u.gameRepo.Remove(roomName)
+	return err
+}
+
+func (u *RoomUsecase) LeaveAllRoom(s socketio.Conn) error {
+	for _, roomName := range s.Rooms() {
+		u.roomRepo.LeaveRoom(s, roomName)
+		err := u.gameRepo.Remove(roomName)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
