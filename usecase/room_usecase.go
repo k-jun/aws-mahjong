1a545d28f368ec2b9bbc5a2806129b2de17d5576
keijun.kumagai <keijun091221@gmail.com>
2020-07-24 17:38:46 +0900

    feat(usecase): update usecase following repository

--- usecase/room_usecase.go
+++ usecase/room_usecase.go
@@ -3,12 +3,7 @@ package usecase
 import (
 	"aws-mahjong/game"
 	"aws-mahjong/repository"
-	"aws-mahjong/server/event"
 	"errors"
-	"fmt"
-	"sort"
-
-	socketio "github.com/googollee/go-socket.io"
 )
 
 var (
@@ -18,169 +13,77 @@ var (
 )
 
 type RoomUsecase interface {
-	Rooms() []*RoomInfo
-	Room(roomName string) (*RoomInfo, error)
-	NewRoomStatus(roomName string, payload string)
-	CreateRoom(s socketio.Conn, username string, roomName string, roomCapacity int) error
-	JoinRoom(s socketio.Conn, username string, roomName string) error
-	LeaveRoom(s socketio.Conn, roomName string) error
-	LeaveAllRoom(s socketio.Conn) error
+	Rooms() []*RoomStatus
+	CreateRoom(userId string, userName string, roomName string, roomCapacity int) error
+	JoinRoom(userId string, userName string, roomName string) (*RoomStatus, error)
+	LeaveRoom(userId string, userName string, roomName string) error
 }
 
 type RoomUsecaseImpl struct {
-	gameRepo repository.GameRepository
-	roomRepo *repository.RoomRepository
+	roomRepo repository.RoomRepository
 }
 
-func NewRoomUsecase(roomRepo *repository.RoomRepository, gameRepo repository.GameRepository) RoomUsecase {
-	return &RoomUsecaseImpl{
-		gameRepo: gameRepo,
-		roomRepo: roomRepo,
-	}
+func NewRoomUsecase(roomRepo repository.RoomRepository) RoomUsecase {
+	return &RoomUsecaseImpl{roomRepo: roomRepo}
 }
 
-type RoomInfo struct {
+type RoomStatus struct {
 	Name     string
 	Len      int
 	Capacity int
 }
 
-func (u *RoomUsecaseImpl) Rooms() []*RoomInfo {
-	rooms := []*RoomInfo{}
+func (u *RoomUsecaseImpl) Rooms() []*RoomStatus {
+	rooms := []*RoomStatus{}
+	for key, game := range u.roomRepo.Rooms() {
+		rooms = append(rooms, &RoomStatus{
+			Name:     key,
+			Len:      len(game.Users()),
+			Capacity: game.Capacity(),
+		})
 
-	roomNames := u.roomRepo.Rooms()
-
-	sort.Slice(roomNames, func(i int, j int) bool { return roomNames[i] < roomNames[j] })
-
-	for _, roomName := range roomNames {
-		r, err := u.Room(roomName)
-		if err != nil {
-			continue
-		}
-		rooms = append(rooms, r)
 	}
 	return rooms
 }
 
-func (u *RoomUsecaseImpl) Room(roomName string) (*RoomInfo, error) {
+func (u *RoomUsecaseImpl) Room(roomName string) (*RoomStatus, error) {
 
-	g, err := u.gameRepo.Find(roomName)
+	g, err := u.roomRepo.Find(roomName)
 	if err != nil {
 		return nil, RoomNotFound
 	}
-	foundRoom := &RoomInfo{
+	foundRoom := &RoomStatus{
 		Name:     roomName,
-		Len:      u.roomRepo.RoomLen(roomName),
+		Len:      len(g.Users()),
 		Capacity: g.Capacity(),
 	}
 
 	return foundRoom, nil
 }
 
-func (u *RoomUsecaseImpl) NewRoomStatus(roomName string, payload string) {
-	u.roomRepo.BroadcastToRoom(roomName, event.NewRoomStatus, payload)
-}
-
-func (u *RoomUsecaseImpl) CreateRoom(s socketio.Conn, username string, roomName string, roomCapacity int) error {
-
-	if u.roomRepo.RoomLen(roomName) != 0 {
-		return RoomAlraedyTakenErr
-	}
-
-	user := &game.User{ID: s.ID(), Name: username}
+func (u *RoomUsecaseImpl) CreateRoom(userId string, userName string, roomName string, roomCapacity int) error {
+	user := &game.User{ID: userId, Name: userName}
 	newGame, err := game.NewGame(roomCapacity, user)
 	if err != nil {
 		return err
 	}
-	err = u.gameRepo.Add(roomName, newGame)
+	err = u.roomRepo.Add(roomName, newGame)
 	if err != nil {
 		return err
 	}
-	u.roomRepo.JoinRoom(s, roomName)
 	return nil
 }
 
-func (u *RoomUsecaseImpl) JoinRoom(s socketio.Conn, username string, roomName string) error {
-
-	if u.roomRepo.RoomLen(roomName) == 0 {
-		return RoomNotFound
-	}
-
-	roomGame, err := u.gameRepo.Find(roomName)
+func (u *RoomUsecaseImpl) JoinRoom(userId string, userName string, roomName string) (*RoomStatus, error) {
+	user := &game.User{ID: userId, Name: userName}
+	err := u.roomRepo.AddUserToRoom(roomName, user)
 	if err != nil {
-		return RoomNotFound
+		return nil, err
 	}
-
-	user := &game.User{ID: s.ID(), Name: username}
-	err = roomGame.AddUser(user)
-	if err != nil {
-		return err
-	}
-
-	if u.roomRepo.RoomLen(roomName) >= roomGame.Capacity() {
-		return RoomReachMaxMember
-	}
-	u.roomRepo.JoinRoom(s, roomName)
-
-	if u.roomRepo.RoomLen(roomName) == roomGame.Capacity() {
-		err = roomGame.GameStart()
-		if err != nil {
-			fmt.Println(err)
-			return err
-		}
-
-		u.roomRepo.BroadcastToRoom(roomName, event.GameStart, "")
-
-		// hide from usecase
-		err = roomGame.Board().TurnPlayerTsumo()
-		if err != nil {
-			fmt.Println(err)
-			return err
-		}
-		newGameStatus(u.roomRepo, roomName, roomGame)
-	}
-
-	return nil
+	return u.Room(roomName)
 }
 
-func (u *RoomUsecaseImpl) LeaveRoom(s socketio.Conn, roomName string) error {
-	roomGame, err := u.gameRepo.Find(roomName)
-	if err != nil {
-		return RoomNotFound
-	}
-
-	if u.roomRepo.RoomLen(roomName) == 1 {
-		// last one person leave
-		err = u.gameRepo.Remove(roomName)
-		if err != nil {
-			return err
-		}
-	}
-
-	if roomGame.Board() == nil {
-		// game not started
-		user := &game.User{ID: s.ID()}
-		err = roomGame.RemoveUser(user)
-	} else {
-		// game started
-		err = u.gameRepo.Remove(roomName)
-	}
-
-	if err != nil {
-		return err
-	}
-
-	u.roomRepo.LeaveRoom(s, roomName)
-	return err
-}
-
-func (u *RoomUsecaseImpl) LeaveAllRoom(s socketio.Conn) error {
-	for _, roomName := range s.Rooms() {
-		u.roomRepo.LeaveRoom(s, roomName)
-		err := u.gameRepo.Remove(roomName)
-		if err != nil {
-			return err
-		}
-	}
-	return nil
+func (u *RoomUsecaseImpl) LeaveRoom(userId string, userName string, roomName string) error {
+	user := &game.User{ID: userId, Name: userName}
+	return u.roomRepo.RemoveUserFromRoom(roomName, user)
 }
